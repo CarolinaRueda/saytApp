a02bbca480ce2916801c0f37e26fa0d9a0b7bb30
CarolinaRueda <dianarueda99@gmail.com>
2023-08-10 20:59:15 -0400

    Sing in page

---
+++ src/images/Background.tsx
@@ -0,0 +1,99 @@
+export const Background = () => {
+  return (
+    <svg
+      width="1422"
+      height="998"
+      viewBox="0 0 1422 998"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g filter="url(#filter0_d_8_98)">
+        <ellipse
+          cx="465.839"
+          cy="424.178"
+          rx="101.839"
+          ry="98.1783"
+          fill="#347355"
+          fill-opacity="0.5"
+        />
+        <ellipse
+          cx="1187.57"
+          cy="58.4353"
+          rx="31.57"
+          ry="30.4353"
+          fill="#347355"
+          fill-opacity="0.5"
+        />
+        <ellipse
+          cx="153.156"
+          cy="797"
+          rx="32.1558"
+          ry="31"
+          fill="#347355"
+          fill-opacity="0.5"
+        />
+        <ellipse
+          cx="837.156"
+          cy="154"
+          rx="32.1558"
+          ry="31"
+          fill="#347355"
+          fill-opacity="0.5"
+        />
+        <ellipse
+          cx="1318"
+          cy="940.406"
+          rx="100"
+          ry="96.4057"
+          fill="#347355"
+          fill-opacity="0.5"
+        />
+        <ellipse
+          cx="104"
+          cy="96.6184"
+          rx="100"
+          ry="96.6184"
+          fill="#347355"
+          fill-opacity="0.5"
+        />
+      </g>
+      <defs>
+        <filter
+          id="filter0_d_8_98"
+          x="0"
+          y="0"
+          width="1422"
+          height="1044.81"
+          filterUnits="userSpaceOnUse"
+          color-interpolation-filters="sRGB"
+        >
+          <feFlood flood-opacity="0" result="BackgroundImageFix" />
+          <feColorMatrix
+            in="SourceAlpha"
+            type="matrix"
+            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+            result="hardAlpha"
+          />
+          <feOffset dy="4" />
+          <feGaussianBlur stdDeviation="2" />
+          <feComposite in2="hardAlpha" operator="out" />
+          <feColorMatrix
+            type="matrix"
+            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"
+          />
+          <feBlend
+            mode="normal"
+            in2="BackgroundImageFix"
+            result="effect1_dropShadow_8_98"
+          />
+          <feBlend
+            mode="normal"
+            in="SourceGraphic"
+            in2="effect1_dropShadow_8_98"
+            result="shape"
+          />
+        </filter>
+      </defs>
+    </svg>
+  );
+};
